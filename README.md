87383c3448876d8551a15302af1381ab24a185e1
Fallen_Breath <yqx791125833@gmail.com>
2021-05-16 06:32:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## fabric-mod-template
---
+++ src/main/java/me/fallenbreath/template_mod/mixins/MinecraftServerMixin.java
@@ -0,0 +1,17 @@
+package me.fallenbreath.template_mod.mixins;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin
+{
+	@Inject(method = "run", at = @At("HEAD"))
+	void onRun(CallbackInfo ci)
+	{
+
+	}
+}
