8113cd237b91b31269a716ee6b6049197cdedd3e
Fallen_Breath <yqx791125833@gmail.com>
2021-12-24 23:12:47 +0800

    merging

--- src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/villagerOfferUsesDisplay/MerchantScreenWidgetButtonPageMixin.java
+++ src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/villagerOfferUsesDisplay/MerchantScreenWidgetButtonPageMixin.java
@@ -3,6 +3,9 @@ package me.fallenbreath.tweakermore.mixins.tweaks.villagerOfferUsesDisplay;
 import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
 import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.village.TradeOffer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,27 +21,27 @@ public abstract class MerchantScreenWidgetButtonPageMixin extends ButtonWidget
 
 	@Shadow @Final int index;
 
-	public MerchantScreenWidgetButtonPageMixin(int x, int y, int width, int height, String message, PressAction onPress)
+	public MerchantScreenWidgetButtonPageMixin(int x, int y, int width, int height, Text message, PressAction onPress)
 	{
 		super(x, y, width, height, message, onPress);
 	}
 
 	@Inject(
-			method = "renderToolTip(II)V",
+			method = "renderToolTip(Lnet/minecraft/client/util/math/MatrixStack;II)V",
 			at = @At(
 					value = "FIELD",
 					target = "Lnet/minecraft/client/gui/screen/ingame/MerchantScreen$WidgetButtonPage;x:I",
 					ordinal = 0
 			)
 	)
-	private void renderMaxUsesAmount(int mouseX, int mouseY, CallbackInfo ci)
+	private void renderMaxUsesAmount(MatrixStack matrices, int mouseX, int mouseY, CallbackInfo ci)
 	{
 		if (TweakerMoreConfigs.VILLAGER_OFFER_USES_DISPLAY.getBooleanValue())
 		{
 			if (this.x + 50 <= mouseX && mouseX <= this.x + 65)
 			{
-				TradeOffer offer = this.field_19166.getContainer().getRecipes().get(this.index + ((MerchantScreenAccessor)this.field_19166).getIndexStartOffset());
-				this.field_19166.renderTooltip(String.format("%d / %d", offer.getUses(), offer.getMaxUses()), mouseX, mouseY);
+				TradeOffer offer = this.field_19166.getScreenHandler().getRecipes().get(this.index + ((MerchantScreenAccessor)this.field_19166).getIndexStartOffset());
+				this.field_19166.renderTooltip(matrices, new LiteralText(String.format("%d / %d", offer.getUses(), offer.getMaxUses())), mouseX, mouseY);
 			}
 		}
 	}
