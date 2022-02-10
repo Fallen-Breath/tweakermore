c766e51536850deaadce11ff4878dd80b38e88b8
Fallen_Breath <yqx791125833@gmail.com>
2022-02-10 14:52:13 +0800

    Unified translation key format
    always with a `tweakermore.` prefix

--- src/main/java/me/fallenbreath/tweakermore/config/ConfigFactory.java
+++ src/main/java/me/fallenbreath/tweakermore/config/ConfigFactory.java
@@ -8,7 +8,7 @@ public abstract class ConfigFactory
 {
 	public static ConfigHotkey newConfigHotKey(String name, String defaultHotkey)
 	{
-		return new ConfigHotkey(name, defaultHotkey, name + ".comment");
+		return new ConfigHotkey("tweakermore." + name, defaultHotkey, "tweakermore." + name + ".comment");
 	}
 
 	public static ConfigBooleanHotkeyed newConfigBooleanHotkeyed(String name)
@@ -18,36 +18,36 @@ public abstract class ConfigFactory
 
 	public static ConfigBooleanHotkeyed newConfigBooleanHotkeyed(String name, boolean defaultValue, String defaultHotKey)
 	{
-		return new ConfigBooleanHotkeyed(name, defaultValue, defaultHotKey, name + ".comment", name + ".pretty_name");
+		return new ConfigBooleanHotkeyed("tweakermore." + name, defaultValue, defaultHotKey, "tweakermore." + name + ".comment", "tweakermore." + name + ".pretty_name");
 	}
 
 	public static ConfigBoolean newConfigBoolean(String name, boolean defaultValue)
 	{
-		return new ConfigBoolean(name, defaultValue, name + ".comment");
+		return new ConfigBoolean("tweakermore." + name, defaultValue, "tweakermore." + name + ".comment");
 	}
 
 	public static ConfigInteger newConfigInteger(String name, int defaultValue, int minValue, int maxValue)
 	{
-		return new ConfigInteger(name, defaultValue, minValue, maxValue, name + ".comment");
+		return new ConfigInteger("tweakermore." + name, defaultValue, minValue, maxValue, "tweakermore." + name + ".comment");
 	}
 
 	public static ConfigDouble newConfigDouble(String name, double defaultValue, double minValue, double maxValue)
 	{
-		return new ConfigDouble(name, defaultValue, minValue, maxValue, name + ".comment");
+		return new ConfigDouble("tweakermore." + name, defaultValue, minValue, maxValue, "tweakermore." + name + ".comment");
 	}
 
 	public static ConfigString newConfigString(String name, String defaultValue)
 	{
-		return new ConfigString(name, defaultValue, name + ".comment");
+		return new ConfigString("tweakermore." + name, defaultValue, "tweakermore." + name + ".comment");
 	}
 
 	public static ConfigStringList newConfigStringList(String name, ImmutableList<String> defaultValue)
 	{
-		return new ConfigStringList(name, defaultValue, name + ".comment");
+		return new ConfigStringList("tweakermore." + name, defaultValue, "tweakermore." + name + ".comment");
 	}
 
 	public static ConfigOptionList newConfigOptionList(String name, IConfigOptionListEntry defaultValue)
 	{
-		return new ConfigOptionList(name, defaultValue, name + ".comment");
+		return new ConfigOptionList("tweakermore." + name, defaultValue, "tweakermore." + name + ".comment");
 	}
 }
---
+++ src/main/java/me/fallenbreath/tweakermore/mixins/core/WidgetListConfigOptionsMixin.java
@@ -0,0 +1,26 @@
+package me.fallenbreath.tweakermore.mixins.core;
+
+import fi.dy.masa.malilib.gui.widgets.WidgetListConfigOptions;
+import fi.dy.masa.malilib.util.StringUtils;
+import me.fallenbreath.tweakermore.util.StringUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(WidgetListConfigOptions.class)
+public abstract class WidgetListConfigOptionsMixin
+{
+	@ModifyArg(
+			method = "getMaxNameLengthWrapped",
+			at = @At(
+					value = "INVOKE",
+					target = "Lfi/dy/masa/malilib/gui/widgets/WidgetListConfigOptions;getStringWidth(Ljava/lang/String;)I"
+			),
+			index = 0,
+			remap = false
+	)
+	private String removeTweakerMorePrefix_readConfigBase_has(String name)
+	{
+		return StringUtil.removeTweakerMoreNameSpacePrefix(name);
+	}
+}
