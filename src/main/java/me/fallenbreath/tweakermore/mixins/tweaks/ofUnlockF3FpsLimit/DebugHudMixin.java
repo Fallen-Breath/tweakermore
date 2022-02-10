3a21d045871934bda0285051de1d7e95ba9966c1
Fallen_Breath <yqx791125833@gmail.com>
2022-02-10 23:29:08 +0800

    since version range predicate is supported now, the DebugHudMixin for OF_UNLOCK_F3_FPS_LIMIT doesnt need to be commented out

--- src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/ofUnlockF3FpsLimit/DebugHudMixin.java
+++ src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/ofUnlockF3FpsLimit/DebugHudMixin.java
@@ -1,31 +1,37 @@
 package me.fallenbreath.tweakermore.mixins.tweaks.ofUnlockF3FpsLimit;
 
+import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
 import me.fallenbreath.tweakermore.util.mixin.Condition;
 import me.fallenbreath.tweakermore.util.mixin.ModIds;
 import me.fallenbreath.tweakermore.util.mixin.ModRequire;
 import net.minecraft.client.gui.hud.DebugHud;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @SuppressWarnings("target")
 @ModRequire(enableWhen = {@Condition(ModIds.optifine), @Condition(value = ModIds.minecraft, versionPredicates = ">=1.15")})
 @Mixin(DebugHud.class)
 public abstract class DebugHudMixin
 {
-//	@Dynamic("Added by optifine")
-//	@Shadow(remap = false)
-//	private long updateInfoLeftTimeMs;
-//
-//	@Dynamic("Added by optifine")
-//	@Shadow(remap = false)
-//	private long updateInfoRightTimeMs;
-//
-//	@Inject(method = "render", at = @At("TAIL"))
-//	private void cancelOptFpsLimit(CallbackInfo ci)
-//	{
-//		if (TweakerMoreConfigs.OF_UNLOCK_F3_FPS_LIMIT.getBooleanValue())
-//		{
-//			this.updateInfoLeftTimeMs = 0;
-//			this.updateInfoRightTimeMs = 0;
-//		}
-//	}
+	@Dynamic("Added by optifine")
+	@Shadow(remap = false)
+	private long updateInfoLeftTimeMs;
+
+	@Dynamic("Added by optifine")
+	@Shadow(remap = false)
+	private long updateInfoRightTimeMs;
+
+	@Inject(method = "render", at = @At("TAIL"))
+	private void cancelOptFpsLimit(CallbackInfo ci)
+	{
+		if (TweakerMoreConfigs.OF_UNLOCK_F3_FPS_LIMIT.getBooleanValue())
+		{
+			this.updateInfoLeftTimeMs = 0;
+			this.updateInfoRightTimeMs = 0;
+		}
+	}
 }
