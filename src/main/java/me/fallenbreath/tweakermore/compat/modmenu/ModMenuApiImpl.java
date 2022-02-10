066067dc94c152130c0958cc1415d9ba3db0fad9
Fallen_Breath <yqx791125833@gmail.com>
2022-02-10 16:58:36 +0800

    use old modmenu api for 1.14

--- src/main/java/me/fallenbreath/tweakermore/compat/modmenu/ModMenuApiImpl.java
+++ src/main/java/me/fallenbreath/tweakermore/compat/modmenu/ModMenuApiImpl.java
@@ -1,13 +1,22 @@
 package me.fallenbreath.tweakermore.compat.modmenu;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
+import me.fallenbreath.tweakermore.TweakerMoreMod;
 import me.fallenbreath.tweakermore.gui.TweakerMoreConfigGui;
+import net.minecraft.client.gui.screen.Screen;
 
+import java.util.function.Function;
+
+@SuppressWarnings("deprecation")
 public class ModMenuApiImpl implements ModMenuApi
 {
+    public String getModId()
+    {
+        return TweakerMoreMod.MOD_ID;
+    }
+
     @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory()
+    public Function<Screen, ? extends Screen> getConfigScreenFactory()
     {
         return (screen) -> {
             TweakerMoreConfigGui gui = new TweakerMoreConfigGui();
