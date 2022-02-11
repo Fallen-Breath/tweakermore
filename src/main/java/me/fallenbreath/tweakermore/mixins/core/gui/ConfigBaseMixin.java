7280331a1a1fbdab1ec56c31ebf9ff2caa18883b
Fallen_Breath <yqx791125833@gmail.com>
2022-02-11 14:12:52 +0800

    fixed comment translation not working for 1.18
    due to malilib doesnt directly use the comment content as the translation key

--- src/main/java/me/fallenbreath/tweakermore/mixins/core/gui/ConfigBaseMixin.java
+++ src/main/java/me/fallenbreath/tweakermore/mixins/core/gui/ConfigBaseMixin.java
@@ -5,8 +5,10 @@ import fi.dy.masa.malilib.config.IConfigBase;
 import fi.dy.masa.malilib.config.options.ConfigBase;
 import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -14,6 +16,26 @@ import java.util.List;
 @Mixin(ConfigBase.class)
 public abstract class ConfigBaseMixin
 {
+	@Shadow private String comment;
+
+	@ModifyArg(
+			method = "getComment",
+			at = @At(
+					value = "INVOKE",
+					target = "Lfi/dy/masa/malilib/util/StringUtils;getTranslatedOrFallback(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;"
+			),
+			index = 0,
+			remap = false
+	)
+	private String tweakerMoreConfigCommentIsTheTranslationKey(String key)
+	{
+		if (TweakerMoreConfigs.hasConfig((IConfigBase)this))
+		{
+			key = this.comment;
+		}
+		return key;
+	}
+
 	@Inject(method = "getComment", at = @At("TAIL"), cancellable = true, remap = false)
 	private void appendModRequirementHeader(CallbackInfoReturnable<String> cir)
 	{
