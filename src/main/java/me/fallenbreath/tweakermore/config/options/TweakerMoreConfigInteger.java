07f5e886a8914bdb3f24b8eb71b2a026edf10a4c
Fallen_Breath <yqx791125833@gmail.com>
2022-02-26 00:10:39 +0800

    let onValueChanged works for value changes due to setValueFromJsonElement

--- src/main/java/me/fallenbreath/tweakermore/config/options/TweakerMoreConfigInteger.java
+++ src/main/java/me/fallenbreath/tweakermore/config/options/TweakerMoreConfigInteger.java
@@ -1,5 +1,6 @@
 package me.fallenbreath.tweakermore.config.options;
 
+import com.google.gson.JsonElement;
 import fi.dy.masa.malilib.config.options.ConfigInteger;
 
 public class TweakerMoreConfigInteger extends ConfigInteger implements TweakerMoreIConfigBase
@@ -12,5 +13,18 @@ public class TweakerMoreConfigInteger extends ConfigInteger implements TweakerMo
 	public TweakerMoreConfigInteger(String name, int defaultValue, int minValue, int maxValue)
 	{
 		super(name, defaultValue, minValue, maxValue, TWEAKERMORE_NAMESPACE_PREFIX + name + COMMENT_SUFFIX);
+	}
+
+	@Override
+	public void setValueFromJsonElement(JsonElement element)
+	{
+		int oldValue = this.getIntegerValue();
+
+		super.setValueFromJsonElement(element);
+
+		if (oldValue != this.getIntegerValue())
+		{
+			this.onValueChanged();
+		}
 	}
 }
