d5c2bff3e376d8b41f8060a3a4a55f5bf7d8b07b
Fallen_Breath <yqx791125833@gmail.com>
2022-02-27 02:17:10 +0800

    optifine why

--- src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/ofRemoveSignTextRenderDistance/SignBlockEntityRendererMixin.java
+++ src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/ofRemoveSignTextRenderDistance/SignBlockEntityRendererMixin.java
@@ -6,9 +6,11 @@ import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
 import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.lang.reflect.Field;
+import java.util.function.Supplier;
 
 import static me.fallenbreath.tweakermore.util.ModIds.optifine;
 
@@ -16,10 +18,11 @@ import static me.fallenbreath.tweakermore.util.ModIds.optifine;
 @Mixin(SignBlockEntityRenderer.class)
 public abstract class SignBlockEntityRendererMixin
 {
-	@SuppressWarnings("target")
-	@Dynamic("Added by optifine")
-	@Shadow(remap = false)
-	private static double textRenderDistanceSq;
+// idk why @Shadow does not work
+//	@SuppressWarnings("target")
+//	@Dynamic("Added by optifine")
+//	@Shadow(remap = false)
+//	private static double textRenderDistanceSq;
 
 	@Dynamic("Added by optifine")
 	@Redirect(
@@ -38,6 +41,32 @@ public abstract class SignBlockEntityRendererMixin
 			return Double.MAX_VALUE;
 		}
 		// "vanilla" optifine behavior
-		return textRenderDistanceSq;
+		return textRenderDistanceSqGetter.get();
+	}
+
+	private static final Supplier<Double> textRenderDistanceSqGetter;
+
+	static
+	{
+		try
+		{
+			//noinspection JavaReflectionMemberAccess
+			Field field = SignBlockEntityRenderer.class.getDeclaredField("textRenderDistanceSq");
+			field.setAccessible(true);
+			textRenderDistanceSqGetter = () -> {
+				try
+				{
+					return (double)field.get(null);
+				}
+				catch (Exception e)
+				{
+					throw new RuntimeException("cannot access textRenderDistanceSq");
+				}
+			};
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException("cannot access textRenderDistanceSq, it should be added by Optifine");
+		}
 	}
 }
