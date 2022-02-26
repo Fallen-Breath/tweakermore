8108ca0ac0b84af664bacfb4df95e94e495f5b00
Fallen_Breath <yqx791125833@gmail.com>
2022-02-26 17:10:59 +0800

    Added mod tweak `ofShaderGameTimeAsWorldTime` (boolean)

--- src/main/java/me/fallenbreath/tweakermore/config/TweakerMoreConfigs.java
+++ src/main/java/me/fallenbreath/tweakermore/config/TweakerMoreConfigs.java
@@ -174,6 +174,13 @@ public class TweakerMoreConfigs
 
 	@Config(
 			value = Config.Type.GENERIC,
+			restriction = @Restriction(require = @Condition(optifine)),
+			category = Config.Category.MOD_TWEAKS
+	)
+	public static final TweakerMoreConfigBoolean OF_SHADER_GAME_TIME_AS_WORLD_TIME = newConfigBoolean("ofShaderGameTimeAsWorldTime", false);
+
+	@Config(
+			value = Config.Type.GENERIC,
 			restriction = @Restriction(require = @Condition(replay_mod)),
 			category = Config.Category.MOD_TWEAKS
 	)
