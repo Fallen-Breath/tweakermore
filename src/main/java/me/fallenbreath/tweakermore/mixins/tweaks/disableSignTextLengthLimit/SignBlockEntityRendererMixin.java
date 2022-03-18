c759a42f7c966bd31fae70ba393493daef0142ed
Fallen_Breath <yqx791125833@gmail.com>
2022-03-18 22:20:48 +0800

    fixed SignBlockEntityRendererMixin fails due to optifine

--- src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/disableSignTextLengthLimit/SignBlockEntityRendererMixin.java
+++ src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/disableSignTextLengthLimit/SignBlockEntityRendererMixin.java
@@ -4,13 +4,19 @@ import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
 import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(SignBlockEntityRenderer.class)
 public abstract class SignBlockEntityRendererMixin
 {
+	@SuppressWarnings("UnresolvedMixinReference")
+	@Group(min = 1, max = 1)
 	@ModifyArg(
-			method = "method_3583",  // lambda method in method render
+			method = {  // lambda method in method render
+					"method_3583",  // vanilla
+					"lambda$render$0"  // after being polluted by optifine
+			},
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/client/util/Texts;wrapLines(Lnet/minecraft/text/Text;ILnet/minecraft/client/font/TextRenderer;ZZ)Ljava/util/List;",
