4c583173d12136c52010d7663b2809b037d6d439
Fallen_Breath <yqx791125833@gmail.com>
2022-04-08 18:15:01 +0800

    disableSignTextLengthLimit optifine fix for 14
    optifine causes LocalCapture fails so a separated mixin is required

---
+++ src/main/java/me/fallenbreath/tweakermore/impl/disableSignTextLengthLimit/SignOverflowHintDrawer.java
@@ -0,0 +1,35 @@
+package me.fallenbreath.tweakermore.impl.disableSignTextLengthLimit;
+
+import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
+import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.util.Texts;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+public class SignOverflowHintDrawer
+{
+	public static void drawLineOverflowHint(SignBlockEntity signBlockEntity, TextRenderer textRenderer, int lineIdx, String lineContent)
+	{
+		if (TweakerMoreConfigs.DISABLE_SIGN_TEXT_LENGTH_LIMIT.getBooleanValue())
+		{
+			// make sure it's rendered during SignEditScreen rendering
+			if (signBlockEntity.getCurrentRow() == -1)
+			{
+				return;
+			}
+
+			Text[] texts = signBlockEntity.text;
+			if (0 <= lineIdx && lineIdx < texts.length)
+			{
+				boolean overflowed = Texts.wrapLines(texts[lineIdx], 90, textRenderer, false, true).size() > 1;
+				if (overflowed)
+				{
+					assert Formatting.RED.getColorValue() != null;
+					float xStart = (float)(-textRenderer.getStringWidth(lineContent) / 2);
+					textRenderer.draw("!", xStart - 10, lineIdx * 10 - texts.length * 5, Formatting.RED.getColorValue());
+				}
+			}
+		}
+	}
+}
--- src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/disableSignTextLengthLimit/SignBlockEntityRenderer_optifineMixin.java
+++ src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/disableSignTextLengthLimit/SignBlockEntityRenderer_optifineMixin.java
@@ -4,6 +4,7 @@ import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
 import me.fallenbreath.tweakermore.impl.disableSignTextLengthLimit.SignOverflowHintDrawer;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
@@ -17,17 +18,13 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import static me.fallenbreath.tweakermore.util.ModIds.optifine;
 
-@Restriction(conflict = @Condition(optifine))
+@Restriction(require = @Condition(optifine))
 @Mixin(SignBlockEntityRenderer.class)
-public abstract class SignBlockEntityRendererMixin
+public abstract class SignBlockEntityRenderer_optifineMixin
 {
-	@SuppressWarnings("UnresolvedMixinReference")
-	@Group(min = 1, max = 1)
+	@SuppressWarnings({"UnresolvedMixinReference", "MixinAnnotationTarget"})
 	@ModifyArg(
-			method = {  // lambda method in method render
-					"method_3583",  // vanilla
-					"lambda$render$0"  // after being polluted by optifine
-			},
+			method = "lambda$render$0",  // lambda method in method render after being polluted by optifine
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/client/util/Texts;wrapLines(Lnet/minecraft/text/Text;ILnet/minecraft/client/font/TextRenderer;ZZ)Ljava/util/List;",
@@ -44,6 +41,7 @@ public abstract class SignBlockEntityRendererMixin
 		return maxLength;
 	}
 
+	@Group(name = "lineOverflowHint", min = 1, max = 1)
 	@Inject(
 			method = "render(Lnet/minecraft/block/entity/SignBlockEntity;DDDFI)V",
 			at = @At(
@@ -51,9 +49,9 @@ public abstract class SignBlockEntityRendererMixin
 					target = "Lnet/minecraft/client/font/TextRenderer;draw(Ljava/lang/String;FFI)I",
 					ordinal = 0
 			),
-			locals = LocalCapture.CAPTURE_FAILHARD
+			locals = LocalCapture.CAPTURE_FAILSOFT
 	)
-	private void drawLineOverflowHint(SignBlockEntity signBlockEntity, double xOffset, double yOffset, double zOffset, float tickDelta, int blockBreakStage, CallbackInfo ci, TextRenderer textRenderer, float j, int signColor, int lineIdx, String lineContent)
+	private void drawLineOverflowHint(SignBlockEntity signBlockEntity, double xOffset, double yOffset, double zOffset, float tickDelta, int blockBreakStage, CallbackInfo ci, BlockState blockstate, float f, TextRenderer textRenderer, float f1, int i, int lineIdx, String lineContent)
 	{
 		SignOverflowHintDrawer.drawLineOverflowHint(signBlockEntity, textRenderer, lineIdx, lineContent);
 	}
