80e8c2e8bd05342031394a4d89867689eb19aa29
Fallen_Breath <yqx791125833@gmail.com>
2022-11-05 21:46:28 +0800

    fixed disableSlimeBlockBouncing in 1.14 not work for bed

--- versions/1.14.4/src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/mc_tweaks/disableSlimeBlockBouncing/BedBlockMixin.java
+++ versions/1.14.4/src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/mc_tweaks/disableSlimeBlockBouncing/BedBlockMixin.java
@@ -3,7 +3,7 @@ package me.fallenbreath.tweakermore.mixins.tweaks.mc_tweaks.disableSlimeBlockBou
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
-import net.minecraft.block.SlimeBlock;
+import net.minecraft.block.BedBlock;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import static me.fallenbreath.tweakermore.util.ModIds.minecraft;
 
 @Restriction(require = @Condition(value = minecraft, versionPredicates = "<1.15"))
-@Mixin(SlimeBlock.class)
+@Mixin(BedBlock.class)
 public abstract class BedBlockMixin
 {
 	@Redirect(
-			method = "onLandedUpon",
+			method = "onEntityLand",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/entity/Entity;isSneaking()Z"
