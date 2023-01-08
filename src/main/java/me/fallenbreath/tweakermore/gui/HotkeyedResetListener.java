bc73d3e893657fc00c66ef494ddc6b83ecc5ce74
Fallen_Breath <yqx791125833@gmail.com>
2023-01-08 21:53:19 +0800

    Added license header

--- src/main/java/me/fallenbreath/tweakermore/gui/HotkeyedResetListener.java
+++ src/main/java/me/fallenbreath/tweakermore/gui/HotkeyedResetListener.java
@@ -1,7 +1,23 @@
-/**
- * Stolen from malilib 1.18 v0.11.4
- * original name: HotkeyedBooleanResetListener
+/*
+ * This file is part of the TweakerMore project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2023  Fallen_Breath and contributors
+ *
+ * TweakerMore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TweakerMore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TweakerMore.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package me.fallenbreath.tweakermore.gui;
 
 import fi.dy.masa.malilib.config.gui.ConfigOptionChangeListenerKeybind;
@@ -11,6 +27,10 @@ import fi.dy.masa.malilib.gui.button.ConfigButtonKeybind;
 import fi.dy.masa.malilib.gui.interfaces.IKeybindConfigGui;
 import fi.dy.masa.malilib.hotkeys.IHotkey;
 
+/**
+ * Stolen from malilib 1.18 v0.11.4
+ * original name: HotkeyedBooleanResetListener
+ */
 public class HotkeyedResetListener extends ConfigOptionChangeListenerKeybind
 {
 	private final IHotkey config;
