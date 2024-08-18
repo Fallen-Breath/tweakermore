8b08ba780f4f68028c24154c4011df52a516c4e4
topi-banana <support@topi-banana.com>
2024-08-19 05:17:01 +0900

    add mc tweak disableHoneyBlockEffect

---
+++ src/main/java/me/fallenbreath/tweakermore/mixins/tweaks/mc_tweaks/disableHoneyBlockEffect/BlockMixin.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the TweakerMore project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2023  Fallen_Breath and contributors
+ *
+ * TweakerMore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TweakerMore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TweakerMore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.fallenbreath.tweakermore.mixins.tweaks.mc_tweaks.disableHoneyBlockEffect;
+
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import me.fallenbreath.tweakermore.config.TweakerMoreConfigs;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static me.fallenbreath.tweakermore.util.ModIds.minecraft;
+
+@Restriction(require = @Condition(value = minecraft, versionPredicates = ">=1.15"))
+@Mixin(Block.class)
+public abstract class BlockMixin
+{
+	@Inject(method = "getJumpVelocityMultiplier", at = @At("HEAD"), cancellable = true)
+	private void getJumpVelocityMultiplier(CallbackInfoReturnable<Float> cir) {
+		if (TweakerMoreConfigs.DISABLE_HONEY_BLOCK_EFFECT.getBooleanValue())
+		{
+			Block self = (Block) (Object) this;
+			if (self == Blocks.HONEY_BLOCK)
+			{
+				cir.setReturnValue(1.0F);
+			}
+		}
+	}
+	@Inject(method = "getVelocityMultiplier", at = @At("HEAD"), cancellable = true)
+	private void getVelocityMultiplier(CallbackInfoReturnable<Float> cir) {
+		if (TweakerMoreConfigs.DISABLE_HONEY_BLOCK_EFFECT.getBooleanValue())
+		{
+			Block self = (Block) (Object) this;
+			if (self == Blocks.HONEY_BLOCK)
+			{
+				cir.setReturnValue(1.0F);
+			}
+		}
+	}
+}
